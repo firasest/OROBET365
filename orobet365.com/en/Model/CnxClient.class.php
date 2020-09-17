d24216b1632e08d7e918fb4c2797dcb13cdf5946
RamySy <45522076+RamySy@users.noreply.github.com>
2020-09-17 15:25:10 +0100

    Fixed Login

--- orobet365.com/en/Model/CnxClient.class.php
+++ orobet365.com/en/Model/CnxClient.class.php
@@ -1,11 +1,11 @@
 <?php
 class CnxClient{
-private $login;
+private $username;
 private $password;
 
         
-function __construct($login,$password){
-$this->login = $login;
+function __construct($username,$password){
+$this->username = $username;
 $this->password = $password;
 
 
@@ -15,7 +15,7 @@ $this->password = $password;
 public function verifier(){ 
 
     include('../includes/connect_db.php');
-    $req = $bdd->query(" SELECT * FROM client WHERE login = '$this->login' AND password = '$this->password'");
+    $req = $bdd->query(" SELECT * FROM client WHERE username = '$this->username' AND password = '$this->password'");
     $resultat = $req->fetch();
 
   if (!$resultat)
@@ -27,7 +27,7 @@ public function verifier(){
 {
        session_start();
        $_SESSION['id'] = $resultat['id'];
-       $_SESSION['login'] = $resultat['login'];
+       $_SESSION['username'] = $resultat['username'];
        $_SESSION['password'] = $resultat['password'];
        $_SESSION['email'] = $resultat['email'];
        
@@ -42,7 +42,7 @@ public function verifier(){
 }
 
 
-$client = new CnxClient($_POST['login'],$_POST['password']);
+$client = new CnxClient($_POST['username'],$_POST['password']);
 
 
 ?>
